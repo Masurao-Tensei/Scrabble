f9ce51582895a270fcb87d9d15539d11c1edf588
Eoth <32389782+Eoth@users.noreply.github.com>
2017-11-10 10:25:15 +0100

    Add files via upload

---
+++ VerifMot.java
@@ -0,0 +1,40 @@
+package Dictionnaire;
+
+import java.io.*;
+
+public class VerifMot {
+
+
+	public String dico(String mot) {
+		File fichier = new File ("Dico.txt");
+	    int n = 0;
+
+
+			try{
+				InputStream ips=new FileInputStream(fichier); 
+				InputStreamReader ipsr=new InputStreamReader(ips);
+				BufferedReader br=new BufferedReader(ipsr);
+				String ligne;
+				while (((ligne=br.readLine())!=null)){
+					
+					if(mot.equalsIgnoreCase(ligne))
+					{
+						n=1;
+					}
+					
+					
+				}
+				br.close(); 
+			}		
+			catch (Exception e){
+				System.out.println(e.toString());
+			}
+			
+			if(n==1)
+			{
+				return "Mot correcte";
+			}
+			return  "Mot n'existe pas";
+			
+	}
+}
