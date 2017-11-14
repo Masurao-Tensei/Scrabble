612a243b2536eecf69b43f0edcea18bca408303b
ChloeDelahaye <32202855+ChloeDelahaye@users.noreply.github.com>
2017-11-14 13:35:14 +0100

    Update Plateau1.java

--- Scrabble/src/Plateau1.java
+++ Scrabble/src/Plateau1.java
@@ -8,21 +8,52 @@ import javax.swing.JPanel;
 public class Plateau1 extends JFrame {
 	
 	public Plateau1(){
-		//définit un titre
+		//dÃ©finit un titre
 		this.setTitle("Plateau de scrabble");
-		//rend visible la fenêtre
+		//rend visible la fenÃªtre
 		this.setVisible(true);
-		//définit la taille
+		//dÃ©finit la taille
 		this.setSize(1000, 1000);
 		//positionne au centre
 		this.setLocationRelativeTo(null);
-		//ferme la fenêtre avec la croix
+		//ferme la fenÃªtre avec la croix
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//ajoute les carrés de la classe case
+		//ajoute les carrÃ©s de la classe case
 		this.setContentPane(new Case());
 		
 		
 		
 	}    
+	
+	public void plateauconsole(String tab[][]){
+		
+		
+		
+		for(int i=0;i<15;i++){
+			
+			for(int j=0;j<15;j++){
+			System.out.print(tab[i][j]+"  |");	
+			}
+			
+			System.out.println("\n---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
+		}
+	}
+
+
+
+boolean vide(String[][] t) {
+	int n = 0;
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 15; j++) {
+			if (t[i][j] != " ") {
+				n = 1;
+			}
+		}
+	}
+	if (n == 1)
+		return false;
+	else
+		return true;
+}
 	      
 }
